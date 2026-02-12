b394cdc01e105e0169ea5c947a25947f3d7b14a9
Nikos Livathinos <nli@zurich.ibm.com>
2026-02-12 15:45:59 +0100

    feat(docling-metrics-text): Python implementation of the EditDistanceMetric
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- packages/docling-metrics-text/docling_metrics_text/docling_metrics_text.py
+++ packages/docling-metrics-text/docling_metrics_text/docling_metrics_text.py
@@ -1,38 +1,39 @@
+import nltk
 from docling_metrics_core.base_types import (
     BaseInputSample,
+    BaseMetric,
     BaseSampleResult,
 )
+from nltk import edit_distance, word_tokenize
 
-from . import docling_metrics_text_cpp  # type: ignore
+# Download the NLTK data
+nltk.download("popular", quiet=True)
+nltk.download("punkt_tab", quiet=True)
+
+
+class TextPairSample(BaseInputSample):
+    text_a: str
+    text_b: str
 
 
 class EditDistanceSampleEvaluation(BaseSampleResult):
     edit_distance: float
 
 
-# TODO: Change the signature of the base class
-# class EditDistanceMetric(BaseMetric):
-class EditDistanceMetric:
+class EditDistanceMetric(BaseMetric):
     r"""
-    Expose the C++ edit distance metric as a Python module.
+    Edit distance metric is normalized Levenshtein over the TreeBank tokens of the 2 texts
     """
 
     def __init__(self) -> None:
         r""" """
-        self._manager = docling_metrics_text_cpp.TextManager()
 
     def evaluate_sample(
         self,
-        sample: BaseInputSample,
+        sample: TextPairSample,
     ) -> EditDistanceSampleEvaluation:
-        # TODO: Add the implementation
-        id = sample.id
-        edit_distance = -1
-        result = EditDistanceSampleEvaluation(id=id, edit_distance=edit_distance)
+        tokens_a = word_tokenize(sample.text_a)
+        tokens_b = word_tokenize(sample.text_b)
+        ed_metric = edit_distance(tokens_a, tokens_b)
+        result = EditDistanceSampleEvaluation(id=sample.id, edit_distance=ed_metric)
         return result
-
-    def _tokenize(self, text: str, convert_parentheses: bool = False) -> list[str]:
-        r"""
-        Tokenize the given string
-        """
-        return self._manager.tokenize(text, convert_parentheses)
