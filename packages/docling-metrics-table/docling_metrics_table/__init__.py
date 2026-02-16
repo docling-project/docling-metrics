d89dbf2635474ae63a5e3bd95f43dac26b4ffa3a
Nikos Livathinos <100353117+nikos-livathinos@users.noreply.github.com>
2026-02-16 17:35:39 +0100

    fix: Rename docling-metrics-teds as docling-metrics-table (#11)
    chore: Rename docling-metrics-teds as docling-metrics-table
    
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

---
+++ packages/docling-metrics-table/docling_metrics_table/__init__.py
@@ -0,0 +1,15 @@
+from docling_metrics_table.docling_metrics_table import (
+    TableMetric,
+    TableMetricBracketInputSample,
+    TableMetricDatasetEvaluation,
+    TableMetricHTMLInputSample,
+    TableMetricSampleEvaluation,
+)
+
+__all__ = [
+    "TableMetric",
+    "TableMetricBracketInputSample",
+    "TableMetricDatasetEvaluation",
+    "TableMetricHTMLInputSample",
+    "TableMetricSampleEvaluation",
+]
